c45e2af15f8a4047dcad33d2c25ef4e7880b7ef2
Seu Nome <seu-email@example.com>
2026-06-06 14:11:52 -0300

    Initial commit - back-Suivia project

---
+++ src/main/java/com/suivia/SuiviaApplication.java
@@ -0,0 +1,11 @@
+package com.suivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuiviaApplication.class, args);
+    }
+}
